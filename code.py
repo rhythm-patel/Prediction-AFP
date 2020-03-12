8933871cdba7746c5d629f290708c8a17f8fbde6
Mohnish Agrawal <43900817+Mohnish-Agrawal@users.noreply.github.com>
2020-03-13 02:02:50 +0400

    Add files via upload
    Calculating percentages of each amino acid in sequence in test.csv

---
+++ code.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+dataset = pd.read_csv("train.csv")
+x = dataset.iloc[:,1]
+y = dataset.iloc[:,2]
+
+s = {'A':1,'R':2,'N':3,'D':4,'C':5,'E':6,'Q':7,'G':8,'H':9,'I':10,'L':11,'K':12,'M':13,'F':14,'P':15,'S':16,'T':17,'W':18,'Y':19,'V':20}
+
+listOfPercentages = list()
+
+for rows in y:
+    ls = [0]*20
+    for a in rows:
+        ls[s[a]-1] += 1
+    for i in range(20):
+        ls[i] =  ls[i]*100/len(rows)
+    listOfPercentages.append(ls)
+
+npA = np.array(listOfPercentages)
+for el in x:
+    print(el)
+# print(listOfPercentages)
