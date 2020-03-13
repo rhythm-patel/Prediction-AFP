37c041c5b63a5a699b74f06248d78f8c87f9235f
Mohnish Agrawal <43900817+Mohnish-Agrawal@users.noreply.github.com>
2020-03-13 22:17:04 +0400

    Update code.py

--- code.py
+++ code.py
@@ -19,14 +19,60 @@ for rows in y:
         ls[i] =  ls[i]*100/len(rows)
     listOfPercentages.append(ls)
 
-for l in range(len(x)):
-    listOfPercentages[l].append(x[l])
+# for l in range(len(x)):
+#     listOfPercentages[l].append(x[l])
 
-np.savetxt('output.csv',listOfPercentages,delimiter=",")
+# np.savetxt('output.csv',listOfPercentages,delimiter=",")
+# # print(listOfPercentages)
+
+# dataset = pd.read_csv("output.csv")
+
+# x = dataset.iloc[:,20]
+# for rows in x:
+#     print(rows)
+
+for rows in testSet:
+    ls = [0]*20
+    for a in rows:
+        ls[s[a]-1] += 1
+    for i in range(20):
+        ls[i] = ls[i]/len(rows)
+    listOfPercentagesOut.append(ls)
+
+
+
+# np.savetxt('output.csv',listOfPercentages,delimiter=",")
 # print(listOfPercentages)
 
-dataset = pd.read_csv("output.csv")
+# dataset = pd.read_csv("output.csv")
 
-x = dataset.iloc[:,20]
-for rows in x:
-    print(rows)
+# x = dataset.iloc[:,20]
+# for rows in x:
+#     print(rows)
+
+model = SVC(gamma=30, kernel = "rbf")
+
+model.fit(listOfPercentages,np.ravel(x))
+predictedOut = model.predict(listOfPercentagesOut)
+
+# count = 0
+# for rows in range(len(x)):
+#     if predictedOut[rows] == x[rows]:
+#         count += 1
+
+# print(count/len(x))
+
+
+# print(len(out))
+print(len(predictedOut))
+output = [["ID","Label"]]
+for elements in range(len(predictedOut)):
+    o = []
+    o.append(sampleset[elements])
+    o.append(predictedOut[elements])
+    output.append(o)
+
+with open('submission.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(output)
+
