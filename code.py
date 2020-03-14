7965a394d6219f2099acc2d269a1bef4c1e2c35d
Rhythm Patel <rhythmpatel200@gmail.com>
2020-03-14 17:20:40 +0530

    Added Accuracy tests

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import csv
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
+from sklearn.model_selection import KFold
 
 s = {'A':1,'R':2,'N':3,'D':4,'C':5,'E':6,'Q':7,'G':8,'H':9,'I':10,'L':11,'K':12,'M':13,'F':14,'P':15,'S':16,'T':17,'W':18,'Y':19,'V':20}
 
@@ -29,50 +30,76 @@ def findPercentageInSeries(ser):
 
     return percentages
 
+def fitModel(X_train,y_train):
+    model = SVC(gamma=30, kernel = "rbf") # our ML model
+    model.fit(X_train,y_train) # fit the model by x & y of train
+    return model
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+def findAccuracy():
 
-x_train_peptide = train.iloc[:,2] # peptide sequence of train dataset
-y_train = train.iloc[:,1] # 1/-1 of train dataset
-x_test_peptide = test.iloc[:,1] # peptide sequence of test dataset
-IDs = test.iloc[:,0] # IDs from the test dataset
+    trainDataset = pd.read_csv("train.csv")
+    test = pd.read_csv("test.csv")
 
-x_train = findPercentageInSeries(x_train_peptide)
-x_test = findPercentageInSeries(x_test_peptide)
+    x_train_peptide = trainDataset.iloc[:,2] # peptide sequence of train dataset
+    
+    X = findPercentageInSeries(x_train_peptide)
+    y = trainDataset.iloc[:,1] # 1/-1 of train dataset
 
-# np.savetxt('output.csv',listOfPercentages,delimiter=",")
-# print(listOfPercentages)
+    noOfSplits = 5
 
-# train = pd.read_csv("output.csv")
+    kf = KFold(n_splits=noOfSplits,random_state=False,shuffle=False)
+    trainAcc = []
+    testAcc = []
 
-# x = train.iloc[:,20]
-# for rows in x:
-#     print(rows)
+    for train_index, test_index in kf.split(X):
+        # print("TRAIN:", train_index, "TEST:", test_index)
+        X = np.asarray(X) # convert list to numpy array
+        y = np.asarray(y) # convert list to numpy array
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-model = SVC(gamma=30, kernel = "rbf") # or ML model
+        model = fitModel(X_train,y_train)
 
-model.fit(x_train,np.ravel(y_train)) # fit the model by x & y of train
-y_test = model.predict(x_test) # predict the y of test based on our model
+        trainAccuracy = model.score(X_train,y_train) # first predicts X_train to y_train' & then compares with y_train
+        testAccuracy = model.score(X_test,y_test)  # first predicts X_test to y_test' & then compares with y_test
 
-# count = 0
-# for rows in range(len(x)):
-#     if y_test[rows] == x[rows]:
-#         count += 1
+        trainAcc.append(trainAccuracy)
+        testAcc.append(testAccuracy)
+        # rmse = sqrt(mean_squared_error(y_test, y_pred))
+        # print (rmse)
 
-# print(count/len(x))
+    print ("Avg Train Accuracy: ",sum(trainAcc)/noOfSplits)
+    print ("Avg Test Accuracy: ",sum(testAcc)/noOfSplits)
 
+def predict():
 
-# print(len(out))
-# print(len(y_test))
+    train = pd.read_csv("train.csv")
+    test = pd.read_csv("test.csv")
 
-output = [["ID","Label"]]
-for i in range(len(y_test)):
-    temp = []
-    temp.append(IDs[i]) #adds the IDs
-    temp.append(y_test[i]) #adds the predicted y values i.e 1/-1
-    output.append(temp)
+    x_train_peptide = train.iloc[:,2] # peptide sequence of train dataset
+    y_train = train.iloc[:,1] # 1/-1 of train dataset
+    x_test_peptide = test.iloc[:,1] # peptide sequence of test dataset
+    IDs = test.iloc[:,0] # IDs from the test dataset
 
-with open('submission.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(output)
+    X_train = findPercentageInSeries(x_train_peptide)
+    X_test = findPercentageInSeries(x_test_peptide)
+
+    y_train = np.asarray(y_train) # convert list to numpy array
+    # y_train = np.ravel(y_train) # reshape 2D array to 1D array
+
+    model = fitModel(X_train,y_train)
+    y_test = model.predict(X_test) # predict the y of test based on our model
+
+    output = [["ID","Label"]]
+    for i in range(len(y_test)):
+        temp = []
+        temp.append(IDs[i]) #adds the IDs
+        temp.append(y_test[i]) #adds the predicted y values i.e 1/-1
+        output.append(temp)
+
+    with open('submission.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(output)
+
+predict()
+findAccuracy()
