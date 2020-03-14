de0fa45ce9ba9ff206953ad94bbaef5cced146cf
Mohnish Agrawal <43900817+Mohnish-Agrawal@users.noreply.github.com>
2020-03-14 11:33:40 +0400

    Update code.py

--- code.py
+++ code.py
@@ -2,34 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
 
 dataset = pd.read_csv("train.csv")
+testset = pd.read_csv("test.csv")
+sample = pd.read_csv("sample.csv")
+
 x = dataset.iloc[:,1]
 y = dataset.iloc[:,2]
+testSet = testset.iloc[:,1]
+sampleset = sample.iloc[:,0]
 
 s = {'A':1,'R':2,'N':3,'D':4,'C':5,'E':6,'Q':7,'G':8,'H':9,'I':10,'L':11,'K':12,'M':13,'F':14,'P':15,'S':16,'T':17,'W':18,'Y':19,'V':20}
 
 listOfPercentages = list()
+listOfPercentagesOut = list()
 
 for rows in y:
     ls = [0]*20
     for a in rows:
         ls[s[a]-1] += 1
     for i in range(20):
-        ls[i] =  ls[i]*100/len(rows)
+        ls[i] =  ls[i]/len(rows)
     listOfPercentages.append(ls)
 
 # for l in range(len(x)):
 #     listOfPercentages[l].append(x[l])
-
-# np.savetxt('output.csv',listOfPercentages,delimiter=",")
-# # print(listOfPercentages)
-
-# dataset = pd.read_csv("output.csv")
-
-# x = dataset.iloc[:,20]
-# for rows in x:
-#     print(rows)
 
 for rows in testSet:
     ls = [0]*20
@@ -75,4 +75,6 @@ for elements in range(len(predictedOut)):
 with open('submission.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(output)
+
+
 
